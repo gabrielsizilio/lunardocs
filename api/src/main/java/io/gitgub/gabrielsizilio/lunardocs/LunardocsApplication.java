6dd270b8c1f1f7a1339feccbc675b835d161a1a8
Gabriel Sizilio <gabrielsizilio15@gmail.com>
2024-12-12 12:45:46 -0300

    chore: LuanrApplication

---
+++ api/src/main/java/io/gitgub/gabrielsizilio/lunardocs/LunardocsApplication.java
@@ -0,0 +1,13 @@
+package io.gitgub.gabrielsizilio.lunardocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunardocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunardocsApplication.class, args);
+	}
+
+}
