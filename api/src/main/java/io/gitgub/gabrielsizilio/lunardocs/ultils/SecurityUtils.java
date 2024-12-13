cd768aee2cf97a5bf630ccdafd3ae0dcad4434f6
Gabriel Sizilio <gabrielsizilio15@gmail.com>
2024-12-13 10:18:02 -0300

    feat: add get user logged

--- api/src/main/java/io/gitgub/gabrielsizilio/lunardocs/ultils/SecurityUtils.java
+++ api/src/main/java/io/gitgub/gabrielsizilio/lunardocs/ultils/SecurityUtils.java
@@ -1,4 +1,19 @@
 package io.gitgub.gabrielsizilio.lunardocs.ultils;
 
+import io.gitgub.gabrielsizilio.lunardocs.domain.user.User;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
 public class SecurityUtils {
+    public static UUID getUserId() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof User) {
+            return ((User) principal).getId();
+        } else {
+            return null;
+        }
+    }
 }
