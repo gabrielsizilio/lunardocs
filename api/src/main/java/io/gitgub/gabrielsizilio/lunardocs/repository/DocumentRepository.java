7a26eecb1e641759ae759484190ba1cd2edcdfa3
Gabriel Sizilio <gabrielsizilio15@gmail.com>
2024-12-13 10:18:36 -0300

    feat: add findDocumentById and OwnerId

--- api/src/main/java/io/gitgub/gabrielsizilio/lunardocs/repository/DocumentRepository.java
+++ api/src/main/java/io/gitgub/gabrielsizilio/lunardocs/repository/DocumentRepository.java
@@ -4,8 +4,11 @@ import io.gitgub.gabrielsizilio.lunardocs.domain.document.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface DocumentRepository extends JpaRepository<Document, UUID> {
     List<Document> findDocumentByOwnerId(UUID ownerId);
+
+    Optional<Document> findDocumentByIdAndOwnerId(UUID idDocument, UUID ownerId);
 }
