d4088a1d0293744ca3f3067b82b48b4cb9fa07b7
Gabriel Sizilio <gabrielsizilio15@gmail.com>
2024-12-13 10:19:58 -0300

    feat: update document response

--- api/src/main/java/io/gitgub/gabrielsizilio/lunardocs/domain/document/dto/DocumentResponseDTO.java
+++ api/src/main/java/io/gitgub/gabrielsizilio/lunardocs/domain/document/dto/DocumentResponseDTO.java
@@ -1,9 +1,8 @@
 package io.gitgub.gabrielsizilio.lunardocs.domain.document.dto;
 
-import io.gitgub.gabrielsizilio.lunardocs.domain.user.User;
 import io.gitgub.gabrielsizilio.lunardocs.domain.user.dto.UserResponseDTO;
 
 import java.util.UUID;
 
-public record DocumentResponseDTO(UserResponseDTO owner, String fileName, String fileDescription, String status) {
+public record DocumentResponseDTO(UUID id, UserResponseDTO owner, String fileName, String fileDescription, String status) {
 }
