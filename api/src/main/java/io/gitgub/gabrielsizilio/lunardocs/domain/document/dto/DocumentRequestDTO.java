0486fa16ddc7b49ae9b6dcdb4470a97170f271ba
Gabriel Sizilio <gabrielsizilio15@gmail.com>
2025-01-02 11:07:37 -0300

    fix: Fix initialization. Update migrations

--- api/src/main/java/io/gitgub/gabrielsizilio/lunardocs/domain/document/dto/DocumentRequestDTO.java
+++ api/src/main/java/io/gitgub/gabrielsizilio/lunardocs/domain/document/dto/DocumentRequestDTO.java
@@ -1,9 +1,4 @@
 package io.gitgub.gabrielsizilio.lunardocs.domain.document.dto;
 
-import io.gitgub.gabrielsizilio.lunardocs.domain.document.StatusDocument;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.UUID;
-
 public record DocumentRequestDTO(String name, String description, String statusDocument) {
 }
